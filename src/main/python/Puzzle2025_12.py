4b0cf474e6e18676733ddd92e48d2df2f3743a35
pareronia <49491686+pareronia@users.noreply.github.com>
2025-04-13 19:28:03 +0200

    Puzzle 12 - python - handle missing locales

--- src/main/python/Puzzle2025_12.py
+++ src/main/python/Puzzle2025_12.py
@@ -4,6 +4,11 @@ import contextlib
 import itertools
 import locale
 import sys
+import unicodedata
+from enum import Enum
+from enum import auto
+from enum import unique
+from functools import cmp_to_key
 from math import prod
 from typing import Any
 from typing import Generator
@@ -32,9 +37,9 @@ O'Neill, Cara: 0109551
 """
 
 Output = int
-DUTCH = "nl_NL"
-ENGLISH = "en"
-SWEDISH = "sv"
+DUTCH, ENGLISH, SWEDISH = "nl_NL", "en", "sv"
+ALFABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+SWEDISH_ALFABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ"
 
 
 @contextlib.contextmanager
@@ -44,6 +49,78 @@ def temp_locale(new_locale: str) -> Generator[Any]:
         yield locale.setlocale(locale.LC_ALL, new_locale)
     finally:
         locale.setlocale(locale.LC_ALL, original_locale)
+
+
+def locale_available() -> bool:
+    cnt = 0
+    for loc in {DUTCH, ENGLISH, SWEDISH}:
+        try:
+            with temp_locale(loc):
+                cnt += 1
+        except locale.Error:
+            pass
+    return cnt == 3
+
+
+@unique
+class Mode(Enum):
+    WITH_LOCALE = auto()
+    WITHOUT_LOCALE = auto()
+
+    def sort(self, entries: list[Entry], loc: str) -> list[Entry]:
+        def cmp_without_locale(e1: Entry, e2: Entry) -> int:
+            def unidecode(s: str) -> str:
+                return "".join(
+                    (ch if ch in ALFABET else "")
+                    for ch in unicodedata.normalize("NFD", s)
+                )
+
+            if loc == SWEDISH:
+                s1, s2 = (
+                    e.last_name(loc).replace("Æ", "Ä").replace("Ø", "Ö")
+                    for e in [e1, e2]
+                )
+                if s1 == s2:
+                    return 0
+                for ch1, ch2 in zip(s1, s2):
+                    if ch1 not in SWEDISH_ALFABET:
+                        ch1 = unidecode(ch1)
+                    if ch2 not in SWEDISH_ALFABET:
+                        ch2 = unidecode(ch2)
+                    if ch1 == ch2:
+                        continue
+                    return SWEDISH_ALFABET.index(ch1) - SWEDISH_ALFABET.index(
+                        ch2
+                    )
+                return len(s1) - len(s2)
+            else:
+                d1, d2 = (
+                    unidecode(
+                        e.last_name(loc)
+                        .replace("Æ", "A")
+                        .replace("Ä", "A")
+                        .replace("Å", "A")
+                        .replace("Ø", "O")
+                    )
+                    for e in [e1, e2]
+                )
+                return 0 if d1 == d2 else -1 if d1 < d2 else 1
+
+        def cmp_with_locale(e1: Entry, e2: Entry) -> int:
+            if loc == SWEDISH:
+                return locale.strcoll(
+                    e1.last_name(loc).replace("Æ", "Ä"),
+                    e2.last_name(loc).replace("Æ", "Ä"),
+                )
+            else:
+                return locale.strcoll(e1.last_name(loc), e2.last_name(loc))
+
+        match self:
+            case Mode.WITH_LOCALE:
+                with temp_locale(loc):
+                    return sorted(entries, key=cmp_to_key(cmp_with_locale))
+            case Mode.WITHOUT_LOCALE:
+                return sorted(entries, key=cmp_to_key(cmp_without_locale))
 
 
 class Entry(NamedTuple):
@@ -63,32 +140,22 @@ class Entry(NamedTuple):
             itertools.dropwhile(lambda ch: ch.islower(), last_name.split())
         )
         return cls(
-            last.replace("'", "").replace(" ", ""),
-            infixes.replace("'", "").replace(" ", ""),
-            first_name.replace("'", "").replace(" ", ""),
+            last.replace("'", "").replace(" ", "").upper(),
+            infixes.replace("'", "").replace(" ", "").upper(),
+            first_name.replace("'", "").replace(" ", "").upper(),
             int(phone),
         )
 
-    def collation_key(self, loc: str) -> str:
-        if loc == DUTCH:
-            return self.last
-        elif loc == SWEDISH:
-            return (self.infixes + self.last).replace("Æ", "Å")
-        else:
-            return self.infixes + self.last
+    def last_name(self, loc: str) -> str:
+        return self.last if loc == DUTCH else self.infixes + self.last
 
 
 class Solution(SolutionBase[Output]):
     def solve(self, input: InputData) -> Output:
-        def sort(loc: str) -> list[Entry]:
-            with temp_locale(loc):
-                return sorted(
-                    entries, key=lambda e: locale.strxfrm(e.collation_key(loc))
-                )
-
+        mode = Mode.WITH_LOCALE if locale_available() else Mode.WITHOUT_LOCALE
         entries = [Entry.from_input(line) for line in input]
         return prod(
-            sort(loc)[len(entries) // 2].phone
+            mode.sort(entries, loc)[len(entries) // 2].phone
             for loc in {ENGLISH, DUTCH, SWEDISH}
         )
 
